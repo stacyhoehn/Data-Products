074b813fc940a207c1201cee41f8b131bb4d3c65
stacyhoehn <slhoehn@gmail.com>
2020-03-06 10:20:18 -0500

    added final project files

---
+++ server.R
@@ -0,0 +1,54 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output,session) {
+    
+    observeEvent(input$mean, {
+        req(input$mean,input$sd)
+        updateSliderInput(session, "range", min=-4*input$sd+input$mean,max=4*input$sd+input$mean,step=0.25*input$sd)
+    })
+    
+    observeEvent(input$sd, {
+        req(input$mean,input$sd)
+        updateSliderInput(session, "range", min=-4*input$sd+input$mean,max=4*input$sd+input$mean,step=0.25*input$sd)
+    })
+    
+    
+    observeEvent(input$mean, {
+        
+        req(input$mean,input$sd,input$range)
+        
+    output$graph <- renderPlot({
+        req(input$mean,input$sd,input$range)
+        min <- input$range[1]
+        max <- input$range[2]
+        mu <- input$mean
+        sigma <- input$sd
+        
+        x <- seq(-4*sigma+mu,4*sigma+mu,length=1000)
+        y <- dnorm(x,mu,sigma)
+        
+        
+        plot(x,y,type="l",xlab="",ylab="",axes=FALSE,main=paste("Normal Distribution with Mean",mu,"and Standard Deviation",sigma))
+        
+        i <- x >= min & x <= max
+        lines(x, y)
+        polygon(c(min,x[i],max), c(0,y[i],0), col="blue")
+        area <- round(pnorm(input$range[2],mu,sigma)-pnorm(input$range[1],mu,sigma),3)
+        mtext(paste("The area under this normal distribution between", min, "and", max, "is",area),side=1,line=4,cex=1.25)
+        axis(1, at=seq(-4*sigma+mu,4*sigma+mu,sigma), pos=0)
+        
+        })
+    
+    }
+)
+})
---
+++ ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Normal Distribution Calculator"),
+
+    headerPanel(h4("This app will compute the area under a normal curve between two specified values.")),
+    
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            
+            numericInput("mean","Enter the mean value for your normal distribution:",value=0),
+            
+            numericInput("sd","Enter the standard deviation for your normal distribution:",value=1,min=1),
+            
+            sliderInput("range","Select your desired range of values:",min=-4,max=4,value=c(-1,1),step=0.25),                                                                      
+            
+        ),
+            # Show a plot of the generated distribution
+        mainPanel(
+            
+            plotOutput("graph")
+        )
+    )
+))
